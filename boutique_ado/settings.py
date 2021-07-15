3b3974a2c8e80c6a8c5368d22b12645b43bbfb8a
JoeyyScott <joey-scott@live.com>
2021-07-15 23:04:35 +0000

    [UPDATE] Updated secret key to use environment variables and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tf7ii6zh74+plcuv2roh)fafzh9g8ioud)xhm235sv&7%cl_(6'
+SECRET_KEY = os.environ.get('SECRET_KEY_BA', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['joeyy-boutique-ado.herokuapp.com', 'localhost']
 
